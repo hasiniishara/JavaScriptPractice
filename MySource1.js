7cb19d6e240b44312d48a9990315cd005b96a55c
HasiniIshara <hasiniisharao4@gmail.com>
2024-02-08 11:06:48 +0200

    completely move functions to MySource1.js file

--- MySource1.js
+++ MySource1.js
@@ -1,4 +1,8 @@
 <script>
+    function myFunction1() {
+        document.getElementById("demo").innerHTML = "Paragraph changed."
+    }
+
     function myFunction2(){
         document.getElementById("test").innerHTML = "Paragraph changed again."
     }
